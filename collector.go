a883750a630cd156767d36f0f2936a2f94bfe52a
Stefan Majewsky <stefan.majewsky@sap.com>
2017-01-13 16:33:45 +0100

    add implementation
    Large parts copied from https://github.com/prometheus/node_exporter @
    commit 9698b97ed11ef2c94f4f9e4d404352dd03dd6bae, hence the attribution
    in the copyright headers.

---
+++ collector.go
@@ -0,0 +1,89 @@
+/*******************************************************************************
+*
+* Copyright 2017 SAP SE
+* Copyright 2015 The Prometheus Authors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+)
+
+var (
+	offset = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "ntp",
+		Name:      "drift_seconds",
+		Help:      "Difference between system time and NTP time.",
+	})
+	stratum = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "ntp",
+		Name:      "stratum",
+		Help:      "Stratum of NTP server.",
+	})
+	scrapeDuration = prometheus.NewSummary(prometheus.SummaryOpts{
+		Namespace: "ntp",
+		Name:      "scrape_duration_seconds",
+		Help:      "ntp_exporter: Duration of a scrape job.",
+	})
+)
+
+//Collector implements the prometheus.Collector interface.
+type Collector struct {
+	NtpServer          string
+	NtpProtocolVersion int
+}
+
+//Describe implements the prometheus.Collector interface.
+func (c Collector) Describe(ch chan<- *prometheus.Desc) {
+	offset.Describe(ch)
+	stratum.Describe(ch)
+	scrapeDuration.Describe(ch)
+}
+
+//Collect implements the prometheus.Collector interface.
+func (c Collector) Collect(ch chan<- prometheus.Metric) {
+	err := c.measure()
+	//only report data when measurement was successful
+	if err == nil {
+		offset.Collect(ch)
+		stratum.Collect(ch)
+		scrapeDuration.Collect(ch)
+	} else {
+		log.Errorln(err)
+		return
+	}
+}
+
+func (c Collector) measure() error {
+	begin := time.Now()
+
+	resp, err := ntp.Query(c.NtpServer, c.NtpProtocolVersion)
+	if err != nil {
+		return fmt.Errorf("couldn't get NTP drift: %s", err)
+	}
+	offset.Set(resp.ClockOffset.Seconds())
+	stratum.Set(float64(resp.Stratum))
+
+	scrapeDuration.Observe(time.Since(begin).Seconds())
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,79 @@
+/*******************************************************************************
+*
+* Copyright 2017 SAP SE
+* Copyright 2015 The Prometheus Authors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
+)
+
+var version string
+
+func main() {
+	var (
+		showVersion        = flag.Bool("version", false, "Print version information.")
+		listenAddress      = flag.String("web.listen-address", ":9100", "Address on which to expose metrics and web interface.")
+		metricsPath        = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+		ntpServer          = flag.String("ntp.server", "", "NTP server to use (required).")
+		ntpProtocolVersion = flag.Int("ntp.protocol-version", 4, "NTP protocol version to use.")
+	)
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	if *ntpServer == "" {
+		log.Fatalln("no NTP server specified, see -ntp.server")
+	}
+	if *ntpProtocolVersion < 2 || *ntpProtocolVersion > 4 {
+		log.Fatalf("invalid NTP protocol version %d; must be 2, 3, or 4", *ntpProtocolVersion)
+	}
+
+	log.Infoln("Starting ntp_exporter", version)
+	prometheus.MustRegister(Collector{*ntpServer, *ntpProtocolVersion})
+	handler := promhttp.HandlerFor(prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{ErrorLog: log.NewErrorLogger()})
+
+	http.Handle(*metricsPath, prometheus.InstrumentHandler("prometheus", handler))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>NTP Exporter</title></head>
+			<body>
+			<h1>NTP Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+
+	log.Infoln("Listening on", *listenAddress)
+	err := http.ListenAndServe(*listenAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
